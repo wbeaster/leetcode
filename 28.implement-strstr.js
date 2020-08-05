b8fab7b007cb56b719f8d00136eabbf4daf309de
Will <williambeaster@gmail.com>
2020-08-05 22:48:19 +0900

    initial commit

---
+++ 28.implement-strstr.js
@@ -0,0 +1,47 @@
+/*
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Example 1:
+
+Input: haystack = "hello", needle = "ll"
+Output: 2
+
+Example 2:
+
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+ 
+
+Constraints:
+
+    haystack and needle consist only of lowercase English characters.
+*/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) 
+{
+    
+};
+
+let haystack = 'hello';
+let needle = 'll';
+
+console.log(`Looking for ${needle} in ${haystack}`);
+
+let firstOcc = strStr(haystack, needle);
+
+console.log(firstOcc);
+
