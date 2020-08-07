10cdecbb66e3c46ec12990b74b9a576a579a6776
Will <williambeaster@gmail.com>
2020-08-07 16:29:02 +0900

    submission cadidate

--- 35.search-insert-position.js
+++ 35.search-insert-position.js
@@ -29,6 +29,35 @@ Output: 0
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
+var searchInsert = function(nums, target) 
+{
+    if (target < nums[0])
+        return 0;
     
+    if (nums.length == 1)
+    {
+        if (nums[0] == target)
+            return 0;
+        if (nums[0] > target)
+            return 0;
+        else 
+            return 1;
+    }
+
+    for (let i=1; i < nums.length; i++)
+    {
+        if (nums[i] == target)
+            return i;
+        else if (nums[i-1] < target && nums[i] > target)
+            return i;
+    }
+
+    return nums.length;
 };
+
+let nums = [1,3,5,6];
+let target = 0;
+
+let answer = searchInsert(nums, target);
+
+console.log(answer);
