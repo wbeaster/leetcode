3dbe4cfcf8da9e2de469e8b271a0d3c74595b95f
Will <williambeaster@gmail.com>
2020-08-09 17:53:01 +0900

    not submitting because the spec is incomplete

--- 58.length-of-last-word.js
+++ 58.length-of-last-word.js
@@ -17,6 +17,34 @@ Output: 5
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
+var lengthOfLastWord = function(s) 
+{
+    //let's get rid of any double spaces to normalize the data
+    //there is clearly some ineffeciencies here
+    //would be more effecient to walk from right to left, 
+    //but that leads to other complexities
+    const twoSpaces = "  ";
+    while (s.includes(twoSpaces))
+    {
+        s = s.replace(twoSpaces, " ");
+    }
+
+    //remove any trailing space, not sure why trim isn't working
+    if (s.lastIndexOf(" ") == s.length-1)
+        s = s.slice(0, s.length-1);
+    
+    let lastSpaceIndex = s.lastIndexOf(" ");
+
+    if (lastSpaceIndex == -1) return 0;
+    
+    return (s.length-1) - lastSpaceIndex;
     
 };
+
+//TEST MORE. YOU ARE TOO TIRED TO SUBMIT THIS RIGHT NOW. REMEMBER, GOAL IS THE HIGHEST NUMBER OF CORRECT SUBMISSIONS
+//let string = "Hello World";
+let string = "World";
+
+let answer = lengthOfLastWord(string);
+
+console.log(answer);
